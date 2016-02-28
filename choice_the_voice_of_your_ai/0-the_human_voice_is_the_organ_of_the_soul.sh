96cf48e90a21c126edf15408ffd69a546e16a39e
Carole Bou <35@p3-2.hbtn.io>
2016-02-27 17:03:49 -0800

    Added comments to explain code

--- choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
+++ choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+# When file is executed, a filename containing the first word of the string entered is created
+# $1 is the first parameter input by user ie message "Hello World"
 filename=$( echo $1 | awk '{print$1}'  )
 
+# $2 is the second parameter input by user ie case m, f or x
 case $2 in
     m)
 	say -v Ralph -o "$filename.m4a" $1
@@ -14,6 +16,7 @@ case $2 in
 	;;
 esac
 
+# $3 is the third parameter input by user ie ip address"
 scp -q $filename.m4a admin@$3:/var/www/html/
 echo "$filename.m4a"
 echo "Listen to the message on http://$3/$filename.m4a"
