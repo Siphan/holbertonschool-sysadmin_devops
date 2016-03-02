ddd907cb633ca0778f314439e966eb150d05ff0f
Carole Bou <35@p3-4.hbtn.io>
2016-03-01 21:33:54 -0800

    Completed tasks 1 and 2

--- read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
+++ read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+file="$1"
+
+head_number=0
+get_number=0
+
+while read -r line || [[ -n "$line" ]]; do
+    head_number_line=$( echo $line | grep -o "HEAD"| wc -l ) # Counting number of head requests
+    get_number_line=$( echo $line | grep -o "GET"| wc -l ) # Counting number of get requests
+    head_number=$(( head_number + head_number_line ))
+    get_number=$(( get_number + get_number_line ))
+done < $file
+
+echo $head_number # Displaying number of head requests
+echo $get_number # Displaying number of get requests
