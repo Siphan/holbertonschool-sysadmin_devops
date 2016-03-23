71f6a7df567282d55105c622f251fcc36c6fe7bb
Carole Bou <35@p4-5.hbtn.io>
2016-03-23 02:21:53 -0700

    Attempt to write script forfun task 1
    Siphan

--- use_a_regular_expression_against_a_problem_now_you_have_2_problems/1-characters_space_characters.rb
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/1-characters_space_characters.rb
@@ -1,1 +1,3 @@
 #!/usr/bin/ruby
+ puts ARGV[0].match(/[a-zA-Z\d]* [a-zA-Z\d]*/) # regexp prints special char '!', need to correct this
+# The regular expression must be exactly matching: characters then a space and then characters and stop matching if special character is encountered
