45547c1564376253939561e1e787668259460530
Carole Bou <35@p3-5.hbtn.io>
2016-03-23 22:32:11 -0700

    Completed for fun tasks
    Siphan

--- use_a_regular_expression_against_a_problem_now_you_have_2_problems/1-characters_space_characters.rb
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/1-characters_space_characters.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/ruby
- puts ARGV[0].match(/[a-zA-Z\d]* [a-zA-Z\d]*/) # regexp prints string after special char '!' encountered, need to correct this
+puts ARGV[0].scan(/(^\w+\s\w+|^\w+\W)/).join # output of scan only includes what is matched by the delimiter |, ie only outputs a word followed by a whitespace and another word. 
 # The regular expression must be exactly matching: characters then a space and then characters and stop matching if special character is encountered
--- use_a_regular_expression_against_a_problem_now_you_have_2_problems/3-textme.rb
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/3-textme.rb
@@ -1,1 +1,5 @@
 #!/usr/bin/ruby
+send = ARGV[0].scan(/(?<=\[from:)\+?\w+/).join # Finds string containing 'from:' and prints word or numbers after it
+rec = ARGV[0].scan(/(?<=\[to:)\+?\w+/).join  # Finds string containing 'to:' and prints word or numbers after it
+flag = ARGV[0].scan(/(?<=\[flags:)[\-?\d\:]+/).join # Finds string containing 'flags:' and prints digits after it
+puts send + "," + rec + "," + flag # Displays above matches to the stdout, separated by commas 
